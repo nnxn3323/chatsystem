77f774447939e41c4a84fa7c57076a1e263e90f7
nnxn3323 <nnxn0418@kakao.com>
2020-10-21 00:50:03 +0900

    ?????

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
-const express = require('express');
-const app = express();
+var express = require('express');
+var app = express();
 
 app.use(express.static('public'));
 app.use(express.static('files'));
 
 app.use('/static', express.static('public'));
 
-app.get('/', function (req, res) {
-  res.send('asdfasdf');
-});
-app.use('/static', express.static(__dirname + '/public'));
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
 
 
-app.listen(8080, function () {
-  console.log('Server is working : PORT - ');
-});
+app.get('/', function(req, res) {
+    res.send('hw,world');
+  });
+  
+  
+  app.listen(app.get('port'), function() {
+    console.log('Node app is running on port', app.get('port'));
+  });
