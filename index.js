960f8e330ce56bd53df5486d6d0c297957189037
nnxn3323 <nnxn0418@kakao.com>
2020-10-21 00:22:20 +0900

    ok

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.use(express.static('public'));
+app.use(express.static('files'));
+
+app.use('/static', express.static('public'));
+
+app.get('/', function (req, res) {
+  res.send('얼촙니당!');
+});
+app.use('/static', express.static(__dirname + '/public'));
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  
+  console.log('Server is working : PORT - ',port);
+});
