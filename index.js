1522d9591de0295304fad5cbfe4f48236a5a4220
nnxn3323 <nnxn0418@kakao.com>
2020-10-21 00:32:35 +0900

    ???

--- index.js
+++ index.js
@@ -11,11 +11,6 @@ app.get('/', function (req, res) {
 });
 app.use('/static', express.static(__dirname + '/public'));
 
-const http = require("http");
-setInterval(function () {
-  http.get("http://chattingdoumi.herokuapp.com");
-}, 600000);
-
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
