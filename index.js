01341765b8d649a78b73d882544b605d9ca3cb10
nnxn3323 <nnxn0418@kakao.com>
2020-10-21 00:43:41 +0900

    ad

--- index.js
+++ index.js
@@ -12,9 +12,6 @@ app.get('/', function (req, res) {
 app.use('/static', express.static(__dirname + '/public'));
 
 
-var server = app.listen(8080, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  
-  console.log('Server is working : PORT - ',port);
+app.listen(8080, function () {
+  console.log('Server is working : PORT - ');
 });
